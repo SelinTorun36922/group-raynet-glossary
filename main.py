d9c695146e76475515f4254d7f40d3578cbb135b
Selint <selintorun36922>
2026-04-26 16:58:19 +0300

    feature-software-terms

---
+++ main.py
@@ -0,0 +1,11 @@
+# Raynet Junior Tech Glossary 
+#Studen A:Selin
+#comment student A = creating the files and adding the git for the project,
+tech_dictionary = { 
+"Python": "A high-level programming language.", 
+"Git": "A distributed version control system." 
+} 
+def list_terms(): 
+    for term, desc in tech_dictionary.items(): 
+        print(f"{term}: {desc}") 
+list_terms() 
